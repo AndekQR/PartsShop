3b8f2f09adfd4e5aa703116ecba2799f8559e61d
DanLeg <daniel.legawiec@britenet.com.pl>
2021-07-07 10:56:34 +0200

    Add form to create new product
    Add frontend controller logic creating new product
    Add image url validating
    Add apex methods to create new product

--- src/aura/PA_CreateProduct/PA_CreateProductController.js
+++ src/aura/PA_CreateProduct/PA_CreateProductController.js
@@ -1,1 +1,28 @@
-({})
+({
+    init: function (component, event, helper) {
+        helper.setProductCategories(component);
+    },
+
+    onSaveProduct: function (component, event, helper) {
+        helper.saveProduct(component);
+    },
+
+    onImageAdd: function (component, event, helper) {
+        let isValid = helper.validateImageUrl(component);
+        if (isValid) {
+            let newImage = component.get('v.image');
+            let allImages = component.get('v.productImages');
+            allImages.push(newImage);
+            component.set('v.productImages', allImages);
+            component.set('v.image', {});
+        }
+    },
+
+    onSpecificationAdd: function(component, event, helper) {
+        let newSpecification = component.get('v.specyfication');
+        let allSpecs = component.get('v.productSpecifications');
+        allSpecs.push(newSpecification);
+        component.set('v.specyfication', {});
+        component.set('v.productSpecifications', allSpecs);
+    },
+})
--- src/aura/PA_CreateProduct/PA_CreateProductHelper.js
+++ src/aura/PA_CreateProduct/PA_CreateProductHelper.js
@@ -1,1 +1,83 @@
-({})
+({
+
+    setProductCategories: function(component) {
+        let product = component.get('v.newProduct');
+        let action = component.get('c.getAllProductCategories');
+        action.setCallback(this, (response) => {
+            let state = response.getState();
+            if(state === 'SUCCESS') {
+                let returnValue = response.getReturnValue();
+                if(returnValue.length > 0) {
+                    product.ProductCategory__c = returnValue[0].Id;
+                    component.set('v.newProduct', product);
+                }
+                component.set('v.allCategories', returnValue);
+            } else {
+                this.handleError(response);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    handleError: function (response) {
+        let errors = response.getError();
+        if (errors) {
+            if (errors[0] && errors[0].message) {
+                console.log("Error message: " +
+                    errors[0].message);
+            }
+        } else {
+            console.log("Unknown error");
+        }
+    },
+
+    validateImageUrl: function(component) {
+        let cmp = component.find('imageUrl');
+        cmp.showHelpMessageIfInvalid();
+        return cmp.get('v.validity').valid;
+    },
+
+    saveProduct: function(component) {
+        let productObject = component.get('v.newProduct');
+        let productImages = component.get('v.productImages');
+        let productSpecifications = component.get('v.productSpecifications');
+
+        productObject.sobjectType = 'Product__c';
+        let specsMap = new Map();
+        productSpecifications.forEach(element => {
+            specsMap[element.Name] = element.Value__c;
+        });
+        console.log(JSON.parse(JSON.stringify(specsMap)));
+        console.log(JSON.parse(JSON.stringify(productObject)));
+        let action = component.get('c.saveProduct');
+        action.setParams({
+            product: productObject,
+            imageUrls: productImages,
+            specs: specsMap
+        });
+        action.setCallback(this, (response) => {
+            let state = response.getState();
+            if(state === 'SUCCESS') {
+                this.clearForm(component);
+                let resultsToast = $A.get("e.force:showToast");
+                resultsToast.setParams({
+                    "title": "Approve Process",
+                    "message": "Product successfully send to approve process.",
+                    "type": "success"
+                });
+                resultsToast.fire();
+            } else {
+                this.handleError(response);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    clearForm: function(component) {
+        component.set('v.newProduct', {});
+        component.set('v.productImages', []);
+        component.set('v.productSpecifications', []);
+        component.set('v.image', '');
+        component.set('v.specyfication', {});
+    }
+})
